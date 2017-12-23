a5437b0e62fee15134843d211dbee41413d2a0ff
arwk <arwk@users.noreply.github.com>
2017-12-23 19:39:24 +0100

    Added content to the login component

--- spa/src/app/login/login.component.ts
+++ spa/src/app/login/login.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  name: string;
+  password: string;
+  loginMessage: string;
+  error: boolean;
+  success: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  signIn(): void {
+    console.log('logging in!')
+    console.log(this.name, this.password);
+    
+    this.error = false;
+    this.loginMessage = "Login successful!";
+
+    this.error = true;
+    this.loginMessage = "Login failed...";
   }
 
 }
