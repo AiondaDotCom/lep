433be16db592e4ddd1cb5fa92b6c23c32344a6bd
arwk <arwk@users.noreply.github.com>
2018-01-09 11:39:44 +0100

    Protect /dashboard and /settings from being accessed, when user is not logged in

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+
+import { AuthGuard } from './auth/auth.guard';
 
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'settings', component: SettingsComponent }//,
-
+  {
+    path: 'dashboard', component: DashboardComponent, canActivate: [
+      AuthGuard
+    ]
+  },
+  {
+    path: 'settings', component: SettingsComponent, canActivate: [
+      AuthGuard
+    ]
+  }//,
   //{ path: 'detail/:id', component: HeroDetailComponent }
 ];
 
@@ -21,6 +29,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
-  declarations: []
+  declarations: [],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
