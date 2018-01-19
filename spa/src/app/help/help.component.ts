b3640a03aba2f5bbf4029176befbe0322269d302
arwk <arwk@users.noreply.github.com>
2018-01-19 15:16:18 +0100

    List whitelisted domains in help.component

--- spa/src/app/help/help.component.ts
+++ spa/src/app/help/help.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Workaround (ERROR: Cannot find name 'require')
+// Source: https://stackoverflow.com/questions/43476135/angular-4-cannot-find-name-require
+// TODO: Better solution
+declare var require: any;
 
 @Component({
   selector: 'app-help',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpComponent implements OnInit {
 
-  constructor() { }
+  fragment: string;
+
+  domainWhitelist: string[];
+
+  constructor(private route: ActivatedRoute) {
+    // TODO: Select correct list, depending on locale
+    this.domainWhitelist = require('../../../../assets/police_domain_names.json')['DE']
+  }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+  }
+
+  ngAfterViewInit(): void {
+    // Source: https://stackoverflow.com/questions/36101756/angular2-routing-with-hashtag-to-page-anchor#36101788
+    // Scrolls to the id corresponding to the fragment
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+    } catch (e) { }
   }
 
 }
