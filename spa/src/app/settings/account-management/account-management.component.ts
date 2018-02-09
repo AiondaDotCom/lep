94a7e9295473e7ac272b0192f3329f5d71398083
arwk <arwk@users.noreply.github.com>
2018-02-09 20:14:03 +0100

    Draft of loginLog list in account settings

--- spa/src/app/settings/account-management/account-management.component.ts
+++ spa/src/app/settings/account-management/account-management.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountManagementComponent implements OnInit {
 
+  loginLogList = [
+    {
+      error: true,
+      message: 'entry 1'
+    },
+    {
+      error: false,
+      message: 'entry 2'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
