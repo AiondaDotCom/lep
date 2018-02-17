52e257446fc8526ded611d9841ee283441e0414e
arwk <arwk@users.noreply.github.com>
2018-02-17 18:01:59 +0100

    Added delete user button to account-management.component; TODO: Ask user for password

--- spa/src/app/settings/account-management/account-management.component.ts
+++ spa/src/app/settings/account-management/account-management.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../auth/auth.service';
 import { ApiService } from '../../api/api.service';
+import { MessageService } from '../../message.service';
 
 @Component({
   selector: 'app-account-management',
@@ -10,7 +14,12 @@ export class AccountManagementComponent implements OnInit {
 
   loginLogList = [];
 
-  constructor(private api: ApiService) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private api: ApiService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
     this.requestLoginLog()
@@ -25,6 +34,29 @@ export class AccountManagementComponent implements OnInit {
       err => {
         console.log(err);
         //this.restrictedContent = err;
+      }
+    );
+  }
+
+  deleteAccount() {
+    let username = 'changeme';
+    let password = 'test';
+    this.api.deleteAccount(username, password).subscribe(
+      result => {
+        console.log(result);
+        this.messageService.message({
+          type: 'info',
+          message: 'Account deleted successfully'
+        })
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      },
+      err => {
+        console.log(err);
+        this.messageService.message({
+          type: 'danger',
+          message: 'Account could not be deleted'
+        })
       }
     );
   }
