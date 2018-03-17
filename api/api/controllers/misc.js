e983dfb632aa219d09933b58a558f2d81bd96958
arwk <arwk@users.noreply.github.com>
2018-03-17 08:15:30 +0100

    Added endpoint /whatIsMyIP

--- api/api/controllers/misc.js
+++ api/api/controllers/misc.js
@@ -5,11 +5,20 @@ var auth = require('../helpers/auth');
 var connection = require('../helpers/db')
 
 module.exports = {
-  getDomainWhitelist: getDomainWhitelist
+  getDomainWhitelist: getDomainWhitelist,
+  whatIsMyIP: whatIsMyIP
 };
 
 function getDomainWhitelist(req, res) {
   // No protection of this endpoint
   let whitelist = auth.getDomainWhitelist();
   res.json(whitelist);
+}
+
+function whatIsMyIP(req, res) {
+  // Returns the ip adress of the client
+  var ip = req.headers['x-forwarded-for'];
+  res.json({
+    ip: ip
+  })
 }
