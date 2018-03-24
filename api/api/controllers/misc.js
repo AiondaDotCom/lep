7441f5c02b7f14a1c971a3d4de51df0613aaf8b0
arwk <arwk@users.noreply.github.com>
2018-03-24 13:53:37 +0100

    Created /status endpoint

--- api/api/controllers/misc.js
+++ api/api/controllers/misc.js
@@ -5,9 +5,20 @@ var auth = require('../helpers/auth');
 var connection = require('../helpers/db')
 
 module.exports = {
+  status: status,
   getDomainWhitelist: getDomainWhitelist,
   whatIsMyIP: whatIsMyIP
 };
+
+function status(req, res) {
+  // TODO: Reaturn correct information
+  res.json({
+    message: 'Status: OK',
+    healthy: true,
+    production: false,
+    setupMode: true
+  })
+}
 
 function getDomainWhitelist(req, res) {
   // No protection of this endpoint
