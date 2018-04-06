6e9222ca2c1b8a6d58c9ea795ecb1c87a97157d5
arwk <arwk@users.noreply.github.com>
2018-04-06 17:08:33 +0200

    Added token input form to setup/auth

--- spa/src/app/setup/auth/auth.component.ts
+++ spa/src/app/setup/auth/auth.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ApiService } from '../../api/api.service';
+import { MessageService } from '../../message.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
 
-  constructor() { }
+export class AuthComponent implements OnInit {
+  setupAuthenticationForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    private api: ApiService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
+    this.setupAuthenticationForm = this.fb.group({
+      'setupToken': ['', Validators.required],
+    });
+  }
+
+  setupAuthentication() {
+    console.log(`Testing token (${this.setupAuthenticationForm.value.setupToken})`)
   }
 
 }
